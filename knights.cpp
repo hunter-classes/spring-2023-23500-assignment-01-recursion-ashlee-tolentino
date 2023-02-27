51de5caa16be98e3a1a83cd3fe6fe33f7f368ed0
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2023-02-26 23:36:37 -0500

    added changes

--- knights.cpp
+++ knights.cpp
@@ -7,71 +7,84 @@ Assignment: Project 1 - Recursive Search (Knights Tour)
 This program uses a 2D array to represent the chess board. Given a starting location, this program will determine a sequence of moves that the knight can take to visit every square on the board without visiting any square more than once.
 */
 
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <unistd.h>
+#include <stdio.h>
+#define K 5
 
-int current_pos = 1;
-int final_pos = 25;
-int path = 0;
+int solve(int x, int y, int I, int maze[K][K], int xList[], int yList[]);
 
-int load_maze(std::string filename, std::string *maze) {
-  std::ifstream infile(filename);
-  int i = 0;
-  while(std::getline(infile, maze[i])){
-    i++;
-  }
-  return i;
+int validIndex(int x, int y, int maze[K][K])
+{
+  return (x >= 0 && x < K && y >= 0 && y < K && maze[x][y] == -1);
 }
 
-void print_maze(std::string maze[], int lines){
-  std::cout << "[0;0H\n";
-
-  for (int i = 0; i < lines; i++){
-    std::cout << maze[i] << "\n";
+void print_maze(int maze[K][K])
+{
+  for (int r = 0; r < K; r++){
+    for (int c = 0; c < K; c++){
+      std::cout << maze[r][c] << ":";
+    }
+    std::cout << std::endl;
   }
-  
 }
 
+int solve1()
+{
+  int maze[K][K];
 
-
-
-void solve(std::string maze[], int lines, int row, int col, bool &solved) {
-  if (maze[row][col] == final_pos){
-    solved = true;
-    return;
-  }
-  if (maze[row][col] == current_pos{
-    return; 
+  for(int r = 0; r < K; r++){
+    for(int c = 0; c < K; c++){
+      maze[r][c] = -1;
+    }
   }
 
-  maze[row][col] = current_pos;
-  usleep(80000);
-  print_maze(maze,lines);
+  int xList[8] = { -2, -2, -1, -1, 1, 1, 2, 2 };
+  int yList[8] = { -1, 1, -2, 2, -2, 2, -1, 1 };
 
-  if (!solved) solve(maze,lines,row-1,col+2, solved);
-  if (!solved) solve(maze,lines,row-1,col-2, solved);
-  if (!solved) solve(maze,lines,row-2,col+1, solved);
-  if (!solved) solve(maze,lines,row-2,col-1, solved);
-  if (!solved) solve(maze,lines,row+1,col+2, solved);
-  if (!solved) solve(maze,lines,row+1,col-2, solved);
-  if (!solved) solve(maze,lines,row+2,col+1, solved);
-  if (!solved) solve(maze,lines,row+2,col-1, solved);
+  maze[0][0] = 1;
 
+  if(solve(0, 0, 2, maze, xList, yList) == 0){
+    std::cout << "No solution";
+    return 0;
+  }
+  else {
+    print_maze(maze);
+  }
+
+  return 1;
 }
 
+int solve(int x, int y, int I, int maze[K][K], int xList[8], int yList[8]) {
+  int k;
+  int nx;
+  int ny;
 
+  if(I > K * K){
+    return 1;
+  }
+
+  for(k = 0; k < 8; k++){
+    nx = x + xList[k];
+    ny = y + yList[k];
+    if(validIndex(nx, ny, maze)){
+      maze[nx][ny] = I;
+      if(solve(nx, ny, I + 1, maze, xList, yList) == 1){
+	return 1;
+      }
+      else{
+	maze[nx][ny] = -1;
+      }
+	 
+    }
+  }
+  return 0;
+}
 
 int main()
-{
-  std::string maze[27];
-  int lines;
-  bool solved = false;
-  
-  lines = load_maze("knights.dat", maze);
-  print_maze(maze,lines);
-  solve(maze,lines,0,0,solved);
+{ 
+  solve1();
   return 0;
 }
